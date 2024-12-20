db8e2c9249c58b6c5114cf3e9157f184e414f9b4
JavierDeSant <j.desantiagosoto@gmail.com>
2024-12-20 03:34:03 +0100

    refactor
    Signed-off-by: JavierDeSant <j.desantiagosoto@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-      <header className="w-full bg-forest-green-600 p-4 text-white text-center">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+      <header className="w-full bg-forest-green-600 p-4 text-center text-white">
         <h1 className="text-3xl md:text-6xl">Spring Sport Flow</h1>
       </header>
-      <main className="flex-1 p-4 flex flex-col items-center justify-center">
-        <div className="max-w-md w-full bg-white p-8 rounded shadow">
-          <h2 className="text-2xl mb-4 text-center">Welcome</h2>
-          <p className="text-center mb-8">Manage your sports activities with ease.</p>
+      <main className="flex flex-1 flex-col items-center justify-center p-4">
+        <div className="w-full max-w-md rounded bg-white p-8 shadow">
+          <h2 className="mb-4 text-center text-2xl">Welcome</h2>
+          <p className="mb-8 text-center">
+            Manage your sports activities with ease.
+          </p>
           <div className="flex flex-col space-y-4">
-            <button className="bg-forest-green text-white py-2 px-4 rounded hover:bg-forest-green-200">
+            <button className="rounded bg-forest-green px-4 py-2 text-white hover:bg-forest-green-200">
               Log In
             </button>
-            <button className="bg-golden-yellow-700 text-white py-2 px-4 rounded hover:bg-golden-yellow-400">
+            <button className="rounded bg-golden-yellow-700 px-4 py-2 text-white hover:bg-golden-yellow-400">
               Register
             </button>
           </div>
         </div>
       </main>
-      <footer className="w-full bg-forest-green-600 p-4 text-white text-center">
+      <footer className="w-full bg-forest-green-600 p-4 text-center text-white">
         <p>&copy; 2023 Spring Sport Flow</p>
       </footer>
     </div>
